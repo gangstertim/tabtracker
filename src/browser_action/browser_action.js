0bd8142cc208851e35521295f20995611013c08f
Tim <timmil@50onred.com>
2014-11-02 23:46:29 -0500

    got bindings to work

--- src/browser_action/browser_action.js
+++ src/browser_action/browser_action.js
@@ -1,13 +1,32 @@
 
 var tabTracker = angular.module('tabTracker', []);
-var tabs;
-chrome.extension.sendRequest({greeting: "hello"}, function(response) {
-    tabs = $.map(response, function(value, index) {
-        return [value];
+
+/*
+tabTracker.controller('TabListCtrl', function($scope) {
+    chrome.extension.sendRequest({greeting: "hello"}, function(response) {
+        $scope.tabs = $.map(response, function(value, index) {
+            return [value];
+        });
+        console.log($scope.tabs);
     });
 });
+*/
 
-setTimeout(tabTracker.controller('TabListCtrl', function($scope) {
-    $scope.tabs = tabs;
-}), 1000);
+tabTracker.controller('TabListCtrl', ['$scope', function($scope) {
+    var tabs;
+    var deferred = $.Deferred();
+    deferred.done(function(tabs) {
+        $scope.tabs = tabs;
+        console.log($scope.tabs);
+        $scope.$apply();
+    });
+
+    chrome.extension.sendRequest({greeting: "hello"}, function(response) {
+        tabs = $.map(response, function(value, index) {
+            return [value];
+        });
+        deferred.resolve(tabs);
+    });
+}]);
+
 
