5c17cfbf80d576154d04b091ba7eb71822f1f5fe
Tim <timmil@50onred.com>
2014-11-04 23:57:59 -0500

    Flushes storage with every call to maintain state, adds instapaper api (doesn't work yet)

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,33 +1,33 @@
 // var settings = new Store("settings", {
-//     "sample_setting": "This is how you use Store.js to remember values"
+//   "sample_setting": "This is how you use Store.js to remember values"
 // });
 
 chrome.storage.sync.clear();
 
 chrome.tabs.onRemoved.addListener(function(tabId) {
-    console.log('removing ' + tabId);
-    chrome.storage.sync.remove(String(tabId));
+  chrome.storage.sync.remove(String(tabId));
 });
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    var toStore = {};
-    toStore[String(tabId)] = { 
-        date: Date(),
-        title: tab.title,
-        url: tab.url
-    };
-        
-    chrome.storage.sync.set(toStore);
+  var toStore = {};
+  toStore[String(tabId)] = { 
+    title: tab.title,
+    date: Date(),
+    url: tab.url
+  };
+    
+  chrome.storage.sync.set(toStore);
 });
 
 chrome.extension.onRequest.addListener(
-    function(request, sender, sendResponse) {
-        chrome.storage.sync.get(null, function(el) {
-            console.log(el);
-            sendResponse(el);
-            return true;
-        });
-    }
+  function(request, sender, sendResponse) {
+    chrome.storage.sync.get(request.currentTabs, function(tabs) {
+        sendResponse(tabs);
+        chrome.storage.sync.clear();
+        chrome.storage.sync.set(tabs);
+    });
+    return true;
+  }
 );
 
 
--- src/browser_action/browser_action.js
+++ src/browser_action/browser_action.js
@@ -1,47 +1,71 @@
+var ApiUrl = 'https://www.instapaper.com/api/add'
 
-var tabTracker = angular.module('tabTracker', []);
+//Move this guy out to a util.js function eventually
 function secondsToString(seconds) {
-    var datestring = '';
-    var numdays = Math.floor((seconds % 31536000) / 86400); 
-    var numhours = Math.floor(((seconds % 31536000) % 86400) / 3600);
-    var numminutes = Math.floor((((seconds % 31536000) % 86400) % 3600) / 60);
-    var numseconds = Math.floor((((seconds % 31536000) % 86400) % 3600) % 60);
-    if (numdays) { datestring += numdays + " days and "; }
-    if (numhours) { datestring += numhours + " hours, "; }
-    if (numminutes) { datestring += numminutes + " minutes, and "; }
-    datestring += numseconds + " seconds";
-    return datestring;
+  var datestring = '';
+  var numdays = Math.floor((seconds % 31536000) / 86400); 
+  var numhours = Math.floor(((seconds % 31536000) % 86400) / 3600);
+  var numminutes = Math.floor((((seconds % 31536000) % 86400) % 3600) / 60);
+  var numseconds = Math.floor((((seconds % 31536000) % 86400) % 3600) % 60);
+  if (numdays) { datestring += numdays + " days and "; }
+  if (numhours) { datestring += numhours + " hours, "; }
+  if (numminutes) { datestring += numminutes + " minutes, and "; }
+  datestring += numseconds + " seconds";
+  return datestring;
 }
 
-tabTracker.controller('TabListCtrl', ['$scope', function($scope) {
-    var tabs;
-    var deferred = $.Deferred();
-    deferred.done(function(tabs) {
-        $scope.tabs = tabs;
-        $scope.$apply();
-    });
+var tabTracker = angular.module('tabTracker', []);
 
-    chrome.extension.sendRequest({greeting: "hello"}, function(response) {
-        var currentDatetime = new Date();
-        tabs = $.map(response, function(value, index) {
-            if (value.title != "New Tab") { //hacky, must fix
-                return [value];
-            }
-        });
-        tabs.sort(function(a, b) {
-            return a.date - b.date;
-        });
-        tabs.forEach(function(tab) {
-            tab.date = currentDatetime.getTime() - new Date(tab.date).getTime();
-            tab.date/= 1000 //convert to seconds
-            tab.date = secondsToString(tab.date);
-        });
-        deferred.resolve(tabs);
+tabTracker.controller('TabListCtrl', ['$scope', function($scope) {
+  var first = $.Deferred(), second = $.Deferred();
+
+  chrome.tabs.query({}, function(tabs) {
+    var currentTabIds = [];
+    tabs.forEach(function(tab) {
+      currentTabIds.push(String(tab.id));
     });
+    first.resolve(currentTabIds)
+  });
+
+  first.done(function(currentTabIds) {
+    chrome.extension.sendRequest({currentTabs: currentTabIds}, function(response) {
+      var currentDatetime = new Date();
+      var tabs = $.map(response, function(value, index) {
+        return [value];
+      });
+
+      tabs.sort(function(a, b) {
+        return a.date - b.date;
+      }).forEach(function(tab) {
+        tab.date = currentDatetime.getTime() - new Date(tab.date).getTime();
+        tab.date/= 1000 //convert to seconds
+        tab.date = secondsToString(tab.date);
+      });
+
+      second.resolve(tabs);
+    });
+  });
+
+  second.done(function(tabs) {
+    $scope.tabs = tabs;
+    $scope.$apply();
+  });
 }]);
 
 $(document).on('click', '.instapaper-clickable', function(e) {
-    console.log(e.target.dataset.url + ' added to instapaper');
-    var url = e.url;
+  console.log(e.target.dataset.url + ' added to instapaper');
+  var data = {
+    username: 'mrtimo',
+    password: 'instapaper25',
+    url: e.target.dataset.url
+  };
+
+  $.post(ApiUrl, data)
+    .done(function() {
+      console.log('boom');
+    }).fail(function() {
+    console.log('fail');
+    });
+
 });
 
